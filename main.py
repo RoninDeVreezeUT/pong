bc1dd55c0b4607652512f3173518941d1f43d4af
RoninDeVreezeUT <r.p.devreeze@student.utwente.nl>
2026-02-11 13:02:31 +0100

    Create pygame sceen

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import random
+import pygame
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    """
+    In Processing, this is where void setup() would be #
+    """
+    size = (800, 800)
+    pygame.init()
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+
+    """
+    Main game loop which will be executed every frame below
+    In Processing, this is where void draw() would be
+    """
+    while True:
+        # Check for pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # If the screen is closed, quit the program
+                pygame.quit()
+        # This draws canvas and elipse
+        screen.fill((255, 255, 255))
+
+        # updates the entire canvas
+        pygame.display.flip()
+        # limits the frame rate
+        clock.tick(60)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
